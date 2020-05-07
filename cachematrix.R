dc64f118c18c3c94f57d1070916ad2d175bced78
NDC <chapuisna@gmail.com>
2020-05-07 19:54:28 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## First function
+# this function does
+# 1. set the value of the matrix
+# 2. get the value of the matrix
+# 3. set the inverse matrix
+# 4. get the inverse matrix
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeinverseMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- inverse
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Return a matrix that is the inverse matrix of 'x'. However, it first checks
+# to see if the inverse matrix has already been built. If so, it gets the 
+#inverse matrix from the cache and skips the computation. Otherwise, it calculates
+#the inverse matrix and sets the new inverse matrix in the cache via the setinverse function.
+Cacheinverse<- function(x = matrix()) {
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
+
+
+### Trials
+#set a matrix
+mat1<- matrix(1:6, nrow=3, ncol=2)
+mat1
+
+mat2<- makeinverseMatrix(mat1)
+mat2<- makeinverseMatrix(mat1)
+#the second time the code is skipped
+
